7c886c2584d541a0b2ce6cb243078bb1b62b9119
LeeWongSnail <wangli_0632@163.com>
2018-04-20 15:27:16 +0800

    add msg forward

---
+++ Runtime_MsgForward/Runtime_MsgForward/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Runtime_MsgForward
+//
+//  Created by LeeWong on 2018/4/20.
+//  Copyright © 2018年 LeeWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Runtime_MsgForward/Runtime_MsgForward/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Runtime_MsgForward
+//
+//  Created by LeeWong on 2018/4/20.
+//  Copyright © 2018年 LeeWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
