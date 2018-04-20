78cab4205afd5332f91241d63bfefc29dc7efb4e
王立 <wangli@meishubao.com>
2018-04-20 10:26:07 +0800

    add .class runtime floder

---
+++ Runtime_MsgSend/Runtime_MsgSend/Class/Father.h
@@ -0,0 +1,16 @@
+//
+//  Father.h
+//  Runtime_Class
+//
+//  Created by LeeWong on 2018/4/16.
+//  Copyright © 2018年 LeeWong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Father : NSObject
+@property (nonatomic, strong) NSString *name;
+
+
+- (void)eat;
+@end
