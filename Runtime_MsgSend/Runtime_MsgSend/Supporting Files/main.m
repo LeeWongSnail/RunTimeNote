a0a93815d7c2b2631e799763d71cb0998113188e
王立 <wangli@meishubao.com>
2018-04-20 10:16:32 +0800

    add runtime msgsender

---
+++ Runtime_MsgSend/Runtime_MsgSend/Supporting Files/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Runtime_MsgSend
+//
+//  Created by LeeWong on 2018/4/20.
+//  Copyright © 2018年 LeeWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Runtime_MsgSend/Runtime_MsgSendTests/Runtime_MsgSendTests.m
@@ -0,0 +1,39 @@
+//
+//  Runtime_MsgSendTests.m
+//  Runtime_MsgSendTests
+//
+//  Created by LeeWong on 2018/4/20.
+//  Copyright © 2018年 LeeWong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Runtime_MsgSendTests : XCTestCase
+
+@end
+
+@implementation Runtime_MsgSendTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
