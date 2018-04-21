1752b447a054babb6fbd50e58fb0550ad7dbb4be
LeeWongSnail <wangli_0632@163.com>
2018-04-21 14:31:31 +0800

    add category and protocol

---
+++ Runtime_ProtocolCategory/Runtime_ProtocolCategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Runtime_ProtocolCategory
+//
+//  Created by LeeWong on 2018/4/21.
+//  Copyright © 2018年 LeeWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Runtime_ProtocolCategory/Runtime_ProtocolCategory/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Runtime_ProtocolCategory
+//
+//  Created by LeeWong on 2018/4/21.
+//  Copyright © 2018年 LeeWong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
