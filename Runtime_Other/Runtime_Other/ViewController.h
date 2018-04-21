b47a9fc653459059ed3cc3f6f49b19255c8ea568
LeeWongSnail <wangli_0632@163.com>
2018-04-21 17:05:29 +0800

    add runtime left

---
+++ Runtime_Other/Runtime_Other/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Runtime_Other
+//
+//  Created by LeeWong on 2018/4/21.
+//  Copyright © 2018年 LeeWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
